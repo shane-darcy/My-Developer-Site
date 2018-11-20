6a303b652b450cc3442427792e2ef17f647ff622
Shane Darcy <unhoodwinked@Shanes-Air.lan>
2018-11-20 00:50:10 -0500

    finally got my button to pass state to the component

--- src/components/drop-down-arrow.js
+++ src/components/drop-down-arrow.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import { Link } from 'gatsby'
 import dropDown from "../images/drop-down-arrow.svg"
 
 import "../styles/main.scss"
@@ -7,14 +6,10 @@ import "../styles/main.scss"
 class DropDownArrow extends Component {
   render() {
     return (
-      <div style={{margin: `0 auto`}}>
-        <Link to="/page-2/">
-          <button className="drop-down">
-            <img className="drop-down__arrow" src={dropDown} alt="drop down arrow"/>
-            <span className="drop-down__hidden-text">click to learn more about Shane Darcy</span>
-          </button>
-        </Link>
-      </div>
+        <button className="drop-down" onClick={this.props.businessClick}>
+          <img className="drop-down__arrow" src={dropDown} alt="drop down arrow"/>
+          <span className="drop-down__hidden-text">click to learn more about Shane Darcy</span>
+        </button>
     )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React, { Component } from 'react'
+import Layout from '../layouts/layout'
+import { Link } from 'gatsby'
 import Header from '../components/header'
 import Title from '../components/title'
 import ContactMe from '../components/contact-me'
@@ -8,27 +9,55 @@ import Business from '../components/business'
 
 import "../styles/main.scss"
 
-const IndexPage = () => (
- 
-<Layout>
+class IndexPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {showComponent: false};
+        this.handleBusinessClick = this.handleBusinessClick.bind(this);
+    }
+  
+    // function to handle the click
+    handleBusinessClick() {
+        this.setState(prevState => ({
+            showComponent: !prevState.showComponent
+        }));
+    }
 
-    <div>
-    <Header />
-        <div className="home__content-container">
-            <div className="home__title-container">
-                <Title />
-                <DropDownArrow />
-            </div>
-            <div className="home__text-container">
-                <p className="home__text">i build professional websites for small to medium size businesses</p>
-                <p className="home__text">driven impressions in the digital age</p>
-                <p className="home__text">position your brand above</p>
-            </div>
-            < ContactMe />
-        </div>
-    </div>
-    <Link to="/page-2/"></Link>
-</Layout>
-)
+    render() {
+        
+        // the return() to put your default HTML into the DOM
+        return (
+
+            <Layout>
+
+                <div>
+                    <Header />
+                    <div className="home__content-container">
+                        <div className="home__title-container">
+                            <Title />
+                            <DropDownArrow 
+                                style={{margin: `0 auto`}} 
+                                businessClick={this.handleBusinessClick}
+                            />
+                        </div>
+                        {
+                            this.state.showComponent ?
+                                <div>
+                                    <div className="home__text-container">
+                                        <p className="home__text">i build professional websites for small to medium size businesses</p>
+                                        <p className="home__text">driven impressions in the digital age</p>
+                                        <p className="home__text">position your brand above</p>
+                                    </div>
+                                    <ContactMe />
+                                </div>
+                            :
+                            <Business />
+                        }
+                    </div>
+                </div>
+            </Layout>
+        );
+    }
+}
 
 export default IndexPage
